e0d988ee66ea6ae86a54a650a8e99da97f3f195e
Patel-Pal <patelpal.93130@gmail.com>
2026-07-08 10:30:14 +0530

    404 error fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [
-    tailwindcss(),
-    tanstackStart({
-      server: { entry: "server" },
-    }),
-    react(),
-  ],
+  plugins: [tailwindcss(), tanstackStart({ server: { entry: "server" } }), nitro(), react()],
 });
